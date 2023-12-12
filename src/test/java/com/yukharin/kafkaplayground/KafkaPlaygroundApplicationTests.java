d8f2d0f08064d86dcc05fafaa9c5e34515d24de3
Vladislav Iukharin <vladislav_iukharin@epam.com>
2023-12-12 12:10:21 +0600

    Init project

---
+++ src/test/java/com/yukharin/kafkaplayground/KafkaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukharin.kafkaplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
